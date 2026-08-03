0a0d5dd724578426c39ee204ba4c25f8d0cde186
Karthic DR <kdev514@aucklanduni.ac.nz>
2026-08-03 14:09:38 +1200

    Implement employer login and registration features
    Added employer login and registration functionality, including checks for user credentials and account creation.

--- app.py
+++ app.py
@@ -1,32 +1,52 @@
 import streamlit as st
-from database import save_employee, get_all_employees
+from database import *
+create_table()
+create_employer_table()
 
 st.set_page_config(
     page_title="Employee Management System",
     layout="centered"
 )
 
+if "role" not in st.session_state:
+    st.session_state.role = None
+    if st.session_state.role is None:
+
+    st.title("Employee Management System")
+
+    choice = st.radio(
+        "Login as:",
+        ["Employee","Employer"]
+    )
+
+
+    if choice == "Employee":
+        if st.button("Continue as Employee"):
+            st.session_state.role="employee"
+            st.rerun()
+    if choice == "Employer":
+        if st.button("Continue as Employer"):
+            st.session_state.role="employer"
+            st.rerun()
+
+
+
+if st.session_state.role=="employee":
 st.title("Employee Management System")
 st.subheader("Fill in the employee details below.")
 st.divider()
-
 name = st.text_input("Name")
-
 age = st.text_input("Age")
-
 salary = st.text_input("Salary")
-
 try:
     age = int(age)
     salary = int(salary)
 except ValueError:
     st.error("Age and Salary must be numbers.")
-
 gender = st.selectbox(
     "Gender",
     ["Select Gender", "Male", "Female", "Other"]
 )
-
 nationality = st.text_input("Nationality")
 
 st.divider()
@@ -35,8 +55,7 @@ col1, col2 = st.columns(2)
 with col1:
     save = st.button("Save")
 with col2:
-    get_records = st.button("Get Employee Records")
-
+    Edit_records = st.button("Edit Current Table")
 if save:
     if name == "" or age == "" or salary == "" or gender == "Select Gender" or nationality == "":
         st.error("Please fill all the required fields.")
@@ -44,6 +63,42 @@ if save:
         save_employee(name, age, salary, gender, nationality)
         st.success("Employee saved successfully!")
 
+if st.session_state.role=="employer":
+    st.title("Employer Login")
+    username = st.text_input("User ID")
+   password = st.text_input(
+        "Password",
+        type="password"
+    )
+    if st.button("Login"):
+        user = check_employer(username,password)
+        if user:
+            st.success("Login successful")
+            st.session_state.logged_in=True
+            st.rerun()
+        else:
+            st.error("Invalid username/password")
+
+if st.button("Register New Employer"):
+    new_user = st.text_input("Create User ID")
+    new_password = st.text_input(
+        "Create Password",
+        type="password"
+    )
+    if st.button("Create Account"):
+
+        result = register_employer(
+            new_user,
+            new_password
+        )
+        if result:
+            st.success("Employer account created")
+        else:
+            st.error("Username already exists")
+
+
+if st.session_state.get("logged_in"):
+    st.title("Employer Dashboard")
 if get_records:
     st.subheader("Employee Records")
     employees = get_all_employees()
